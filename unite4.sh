93b7b4a3bf6eb7fa14891ded6cfa1ab08a2bcffb
raviksk <ravikant@indusos.com>
2016-07-11 18:55:06 +0530

    sf

---
+++ unite4.sh
@@ -0,0 +1,46 @@
+#Fetching the aosp code path
+echo "Enter AOSP code path:-"
+read path
+#sync from the git repo
+
+git clone https://github.com/raviksk/dialer_erpo_manifest.git
+curl https://storage.googleapis.com/git-repo-downloads/repo > repo
+chmod 777 repo
+./repo init -u https://github.com/raviksk/dialer_erpo_manifest
+./repo sync
+./repo start riba --all
+
+cp -R indusos $path
+
+cd $path
+
+if [ -d $path"/packages/apps/Dialer" ]; then
+	pwd
+	cd $path"/packages/apps/Dialer"
+	find -name "*.mk" -exec mv {} {}.bak \;
+fi
+if [ -d $path"/packages/apps/InCallUI" ]; then
+	pwd
+	cd $path"/packages/apps/InCallUI"
+	find -name "*.mk" -exec mv {} {}.bak \;
+fi
+if [ -d $path"/packages/apps/Contacts" ]; then
+	pwd
+	cd $path"/packages/apps/Contacts"
+	find -name "*.mk" -exec mv {} {}.bak \;
+fi
+if [ -d $path"/packages/apps/ContactsCommon" ]; then
+	pwd
+	cd $path"/packages/apps/ContactsCommon"
+	find -name "*.mk" -exec mv {} {}.bak \;
+fi
+if [ -d $path"/frameworks/opt/net/ims" ]; then
+	pwd
+	cd $path"/frameworks/opt/net/ims"
+	find -name "*.mk" -exec mv {} {}.bak \;
+fi
+if [ -d $path"/frameworks/opt/telephony" ]; then
+	pwd
+	cd $path"/frameworks/opt/telephony"
+	find -name "*.mk" -exec mv {} {}.bak \;
+fi
